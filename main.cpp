d99699bc261d6dfe1858dfe08ecd62592ef8bb44
Hsealey <112498796+sealey22@users.noreply.github.com>
2022-09-24 22:31:34 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main() {
+
+  //load input file 
+  ifstream inFile; //declare stream variable 
+  inFile.open("NumberData1.txt");
+
+  //load in all data in while loop 
+  while (!inFile.eof())
+    {
+      
+      }
+}
