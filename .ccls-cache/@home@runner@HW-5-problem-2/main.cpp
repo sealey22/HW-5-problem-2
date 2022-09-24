3f7927f4a3cbcf4728ec30d23f2e07ea94401953
Hsealey <112498796+sealey22@users.noreply.github.com>
2022-09-24 22:45:38 +0000

    all changes before displaying results to user

--- .ccls-cache/@home@runner@HW-5-problem-2/main.cpp
+++ .ccls-cache/@home@runner@HW-5-problem-2/main.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+int dataTemp;
+int sumData = 0;
+int counter = 0;
+
 
 int main() {
 
@@ -11,8 +15,30 @@ int main() {
   inFile.open("NumberData1.txt");
 
   //load in all data in while loop 
-  while (!inFile.eof())
+  while (inFile >> dataTemp)
     {
-      
+      //inFile >> dataTemp;
+      //cout << dataTemp << endl;
+
+      //check to see if data is valid
+      if (dataTemp >= 0)
+      {
+        //update cumulative value of all data and number of data points loaded
+        sumData = sumData +dataTemp;
+        counter++;
+        }
+        
+      else // the value is negative and we should exit 
+      {
+        cout << "Negative value encountered of " << dataTemp << "." << endl;
+        cout << "Will not use rest of data..." << endl;
+        break;
+        }
       }
+
+      //display to user 
+      cout << "The sum of the valid data is: " << sumData
+  
+  return 0;
+  
 }
